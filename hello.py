bdd9581947db701be11dfee313c0694c8d23ca8d
fabio-teixeira-ifsp <fabio.teixeira@ifsp.edu.br>
2024-03-21 16:23:35 -0300

    Primeira atualização Aula.060.C

--- hello.py
+++ hello.py
@@ -66,6 +66,7 @@ def internal_server_error(e):
 def index():
     form = NameForm()
     user_all = User.query.all();
+    role_all = Role.query.all();
     print(user_all);
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.name.data).first()                
@@ -81,4 +82,4 @@ def index():
         return redirect(url_for('index'))
     return render_template('index.html', form=form, name=session.get('name'),
                            known=session.get('known', False),
-                           user_all=user_all);
+                           user_all=user_all, role_all = role_all);
